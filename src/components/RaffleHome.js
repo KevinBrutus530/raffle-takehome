6f115c4066d21cd8e51f6f554637e81902f60853
Kevin Brutus <kevinbrutus@pursuit.org>
2021-05-30 13:14:54 -0400

    install react-router-dom, create RaffleHome, and wrap app in Router

---
+++ src/components/RaffleHome.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import cors from "cors";
+import axios from "axios";
+
+const RaffleHome = () => {
+  //   const fetchData = async () => {
+  //     try {
+  //       let res = await axios.get(
+  //         "https://raffle-fs-app.herokuapp.com/api/raffles"
+  //       );
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   useEffect(() => {
+  //     fetchData();
+  //   }, []);
+
+  return (
+    <div>
+      <h1>We In Here</h1>
+    </div>
+  );
+};
+
+export default RaffleHome;
