7da236e88857d9401a3ae3a45b8ac832d23c0a7d
Kevin Brutus <kevinbrutus@pursuit.org>
2021-05-31 14:50:02 -0400

    continue working on PickRaffleWinner

--- src/components/PickRaffleWinner.js
+++ src/components/PickRaffleWinner.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useInput } from "../util/customHooks";
 import axios from "axios";
+import RaffleParticipants from "./RaffleParticipants";
 
 // Create logic that determines if raffle's secret_token matches input
 // Create logic that shuffles raffle participants and selects a winner
@@ -10,19 +11,65 @@ import axios from "axios";
 // STYLE STYLE STYLE!!!!
 
 const PickRaffleWinner = () => {
+  const [raffleSecretToken, setRaffleSecretToken] = useState("");
+  const [raffleParticipants, setRaffleParticipants] = useState([""]);
   const secretKey = useInput("");
   const params = useParams("/raffle/:id");
   const { id } = params;
 
+  useEffect(() => {
+    const fetchRaffle = async () => {
+      try {
+        let res = await axios.get(
+          `https://cors-anywhere.herokuapp.com/https://raffle-fs-app.herokuapp.com/api/raffles/${id}`
+        );
+        setRaffleSecretToken(res.data.secret_token);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchRaffle();
+  }, []);
+
+  useEffect(() => {
+    const fetchRaffleParticipants = async () => {
+      try {
+        let res = await axios.get(
+          `https://cors-anywhere.herokuapp.com/https://raffle-fs-app.herokuapp.com/api/raffles/${id}/participants`
+        );
+        setRaffleParticipants(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchRaffleParticipants();
+  }, []);
+
+  //   const shuffle = (arr) => {
+  //     for (let i = arr.length - 1; i > 0; i--) {
+  //       const j = Math.floor(Math.random() * (i + 1));
+  //       const temp = arr[i];
+  //       arr[i] = arr[j];
+  //       arr[j] = temp;
+  //     }
+  //   };
   const handleRaffleWinner = async (e) => {
     e.preventDefault();
-    try {
-      await axios.post(
-        `https://raffle-fs-app.herokuapp.com/raffle/${id}/winner`,
-        { secret_token: secretKey }
-      );
-    } catch (error) {
-      console.log(error);
+    // const pickWinner =
+    //   raffleParticipants[Math.floor(Math.random() * raffleParticipants.length)];
+    if (secretKey.value === raffleSecretToken) {
+      debugger;
+      try {
+        await axios.put(
+          `https://raffle-fs-app.herokuapp.com/raffle/${id}/winner`,
+          { secret_token: secretKey.value }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      debugger;
+      window.alert("Key incorrect, please try again.");
     }
   };
 
